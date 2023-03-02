0b7315d25bdaef6b320bb25a05f7cc663950a8f3
brenekv.03 <brenekv.03@A321-15>
2023-03-02 11:06:43 +0100

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ciselneSoustavy01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cislo = int.Parse(textBox1.Text);
+            if (JePrvocislo(cislo)) MessageBox.Show("Je prvočíslo\n" + cislo);
+            else MessageBox.Show("Není prvočíslo\n" + cislo);
+        }
+        static private bool JePrvocislo(int cislo)
+        {
+            bool jePrvocislo = true;
+            for(int i =2;i<=cislo/2&&jePrvocislo;i++)
+            {
+                if(cislo%i==0)jePrvocislo=false;
+            }
+            return jePrvocislo;
+        }
+    }
+}
